a1b9ffa869cd460cd814197330f40f41ea291928
FardinKhan5 <fardinkhan52003@gmail.com>
2024-07-05 14:42:31 +0530

    setup express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import connectDB from "./db/index.js"
 import dotenv from "dotenv/config.js"
+import { app } from "./app.js"
 
-connectDB()
+connectDB().then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR " + error)
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`app is listening on port ${process.env.PORT}`)
+    })
+}).catch((err)=>{
+    console.log("mongo db connection failed "+ err)
+})
