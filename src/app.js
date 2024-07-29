643bcde7336695951dc15c364c95254a0d14b866
FardinKhan5 <fardinkhan52003@gmail.com>
2024-07-29 17:11:43 +0530

    add like routes and controllers

--- src/app.js
+++ src/app.js
@@ -17,10 +17,12 @@ import videoRouter from "./routes/video.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
 // use routes
 app.use("/api/v1/users/",userRouter)
 app.use("/api/v1/videos/",videoRouter)
 app.use("/api/v1/subscriptions/",subscriptionRouter)
 app.use("/api/v1/tweets/",tweetRouter)
 app.use("/api/v1/comments/",commentRouter)
+app.use("/api/v1/likes", likeRouter)
 export {app}
--- src/models/like.model.js
+++ src/models/like.model.js
@@ -2,21 +2,27 @@ import mongoose, { Schema } from "mongoose"
 
 const likeSchema = new Schema(
     {
-        comment: {
-            type: Schema.Types.ObjectId,
-            ref: "Comment"
-        },
-        video: {
-            type: Schema.Types.ObjectId,
-            ref: "Video"
-        },
+        comment: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Comment"
+            }
+        ],
+        video: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        tweet: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Tweet"
+            }
+        ],
         likedBy: {
             type: Schema.Types.ObjectId,
             ref: "User"
-        },
-        tweet: {
-            type: Schema.Types.ObjectId,
-            ref: "Tweet"
         }
     },
     {
