09f8d17fab2c1a869c2ada01521462a2b425df6c
FardinKhan5 <fardinkhan52003@gmail.com>
2024-07-31 17:14:31 +0530

    add healthcheck route and controller

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 // use routes
 app.use("/api/v1/users/",userRouter)
 app.use("/api/v1/videos/",videoRouter)
@@ -29,4 +30,5 @@ app.use("/api/v1/comments/",commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 export {app}
---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,13 @@
+import { asyncHandler } from "../utills/asyncHandler.js"
+import { ApiError } from "../utills/ApiError.js"
+import { ApiResponse } from "../utills/ApiResponse.js"
+
+
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    res.status(200).json(new ApiResponse(200,{status:"OK"},"ALL OK"))
+})
+
+export {
+    healthcheck
+    }
